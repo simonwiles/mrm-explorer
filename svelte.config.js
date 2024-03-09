0dac7e4c4c3a6269dfc951b45151bdaf00c9cbaa
Simon Wiles <simonjwiles@gmail.com>
2024-03-08 19:44:07 -0800

    GitHub Pages build tooling

--- svelte.config.js
+++ svelte.config.js
@@ -6,7 +6,16 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter(),
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			precompress: false,
+			strict: true,
+			fallback: '404.html'
+		}),
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		},
 		alias: {
 			'@': './src',
 			'@components': './src/components'
