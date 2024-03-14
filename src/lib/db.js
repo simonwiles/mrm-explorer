e63ac1fc5f187b5c44e826f9c5e9c68b261989d0
Simon Wiles <simonjwiles@gmail.com>
2024-03-13 19:50:29 -0700

    Export some more database functions

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,5 +1,5 @@
 import '$lib/types.js';
-import Dexie from 'dexie';
+import Dexie, { liveQuery } from 'dexie';
 
 export const db = new Dexie('mrmExplorer');
 db.version(1).stores({
@@ -11,8 +11,30 @@ export const fetchAllImageObjects = async () => {
 };
 
 /**
+ * Generates a live query for all image objects.
+ *
+ * @return  {import('dexie').Observable<Array<ImageObject & { id: number }>>} An Observable array of image objects.
+ */
+export const liveQueryAllImageObjects = () => {
+	return liveQuery(() => db.table('images').toArray());
+};
+
+/**
+ * @param {number} id
+ */
+export const fetchImageObjectById = async (id) => {
+	try {
+		return await db.table('images').get(id);
+	} catch (error) {
+		console.error(`Failed to get ID ${id}: ${error}`);
+		return false;
+	}
+};
+
+/**
  * insert or update an image in the database
  * @param {ImageObject} imageObject
+ * @returns {Promise<[boolean, ImageObject | null]>}
  */
 export const upsertImageObject = async (imageObject) => {
 	try {
@@ -21,20 +43,28 @@ export const upsertImageObject = async (imageObject) => {
 			console.log(`Image ${imageObject.name} successfully updated.`);
 			return [false, imageObject];
 		}
+
 		const id = await db.table('images').add(imageObject);
 		console.log(`Image ${imageObject.name} successfully added. Got id ${imageObject.id}`);
-		return [true, { id, ...imageObject }];
+		return [true, { id: /** @type {number} */ (id), ...imageObject }];
 	} catch (error) {
 		console.error(`Failed to upsert ${imageObject.name}: ${error}`);
-		return [false, {}];
+		return [false, null];
 	}
 };
 
 /**
- * Remove an image from the database
- * @param {ImageObject} imageObject
+ * Remove an image object from the database
+ * @param {ImageObject & { id: number }} imageObject
  */
 export const removeImageObject = async (imageObject) => {
-	if (!imageObject.id) return;
 	await db.table('images').delete(imageObject.id);
+};
+
+/**
+ * Remove an image from the database by ID
+ * @param {number} id
+ */
+export const removeImageObjectById = async (id) => {
+	await db.table('images').delete(id);
 };
