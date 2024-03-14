e952729337049707b0a71728d03c008cfc4d9684
Simon Wiles <simonjwiles@gmail.com>
2024-03-13 17:23:14 -0700

    "Landing Page"
    Not really worthy of the name, but adequate for now.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <script>
-	import Viewer from '@components/Viewer.svelte';
+	import AddNewImage from '@components/AddNewImage.svelte';
 </script>
 
 <svelte:head>
@@ -7,4 +7,20 @@
 	<meta name="description" content="Application for investigating MRM outputs" />
 </svelte:head>
 
-<Viewer />
+<section>
+	<h1>MRM Explorer</h1>
+
+	<p>Upload an image to get started:</p>
+
+	<AddNewImage redirectOnAdd={true} />
+</section>
+
+<style>
+	section {
+		margin: 0 0 2rem 2rem;
+
+		& * {
+			margin: 2rem 0;
+		}
+	}
+</style>
