02f05051539890d78df82babff33dc016db35b1e
Simon Wiles <simonjwiles@gmail.com>
2024-03-15 16:25:28 -0700

    `<AddFeaturesFromJson/>` component

---
+++ src/components/AddFeaturesFromJson.svelte
@@ -0,0 +1,57 @@
+<script>
+	import { tick, unstate } from 'svelte';
+	import { FileUploaderDropContainer } from 'carbon-components-svelte';
+	import { upsertImageObject } from '$lib/db';
+
+	/** @type {{imageObject: ImageObject}} */
+	let { imageObject } = $props();
+
+	/**
+	 * @param {{features: Array<Feature>}} mrmGeoJson
+	 */
+	async function addFeaturesToImage(mrmGeoJson) {
+		imageObject.features = mrmGeoJson.features;
+		await tick();
+		// createFeaturePaths(mrmGeoJson.features);
+		upsertImageObject(unstate(imageObject));
+	}
+</script>
+
+<FileUploaderDropContainer
+	labelText="Add JSON"
+	class="add-json"
+	on:change={({ detail: files }) => {
+		const file = files[0];
+		if (!file.type || !['application/geo+json', 'application/json'].includes(file.type)) {
+			alert('Only JSON / GEOJSON files are accepted');
+			return;
+		}
+		const reader = new FileReader();
+		reader.addEventListener(
+			'load',
+			() => typeof reader.result === 'string' && addFeaturesToImage(JSON.parse(reader.result)),
+			false
+		);
+		reader.readAsText(file);
+	}}
+/>
+
+<style>
+	:global(.add-json) {
+		inline-size: auto;
+		overflow: hidden;
+	}
+
+	:global(.add-json .bx--file-browse-btn) {
+		height: 100%;
+		margin: 0;
+	}
+
+	:global(.add-json .bx--file__drop-container) {
+		border: 0;
+		display: grid;
+		height: 100%;
+		padding: 0 1rem;
+		place-content: center;
+	}
+</style>
