c30973e13605862755bf7ef30d60c2c2618e0712
Simon Wiles <simonjwiles@gmail.com>
2024-03-16 22:12:43 -0700

    Deploy from d57220d at 2024-03-16 22:12:42 PDT

---
+++ _app/immutable/chunks/paths.BR9D_JGo.js
@@ -0,0 +1,1 @@
+var s;const a=((s=globalThis.__sveltekit_9xlbby)==null?void 0:s.base)??"/mrm-explorer";var e;const b=((e=globalThis.__sveltekit_9xlbby)==null?void 0:e.assets)??a;export{b as a,a as b};
