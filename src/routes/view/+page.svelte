fe19d8939492183143ae56f3dcc6bc507a53902c
Simon Wiles <simonjwiles@gmail.com>
2024-03-16 17:41:26 -0700

    Build out the view route a little

--- src/routes/view/+page.svelte
+++ src/routes/view/+page.svelte
@@ -1,7 +1,9 @@
 <script>
 	import { page } from '$app/stores';
+	import { InlineNotification, Link, Loading } from 'carbon-components-svelte';
+
 	import { fetchImageObjectById } from '$lib/db';
-	import { Loading } from 'carbon-components-svelte';
+	import { formatBytes } from '@/lib/storage';
 	import Viewer from '@components/Viewer.svelte';
 
 	/** @type {ImageObject | undefined | null} */
@@ -12,6 +14,8 @@
 		let id;
 		if (idStr !== null && (id = parseInt(idStr, 10))) {
 			fetchImageObjectById(id).then((_imageObject) => (imageObject = _imageObject));
+		} else {
+			imageObject = null;
 		}
 	});
 </script>
@@ -21,12 +25,51 @@
 	<meta name="description" content="Application for investigating MRM outputs" />
 </svelte:head>
 
-{#if !idStr}
-	<p>no id?</p>
+{#if !idStr || imageObject === null}
+	<InlineNotification hideCloseButton kind="error" title="Invalid Image ID">
+		<span slot="subtitle">
+			Please specify a valid ID in the URL or
+			<Link inline href="/dataset/">explore the dataset.</Link>.
+		</span>
+	</InlineNotification>
 {:else if imageObject === undefined}
 	<Loading />
-{:else if imageObject === null}
-	<p>bad id?</p>
 {:else}
-	<Viewer {imageObject} />
+	<div class="container">
+		<div class="info">
+			{#if imageObject}
+				<dl>
+					<dt>Name</dt>
+					<dd>{imageObject.name}</dd>
+					<dt>Image Dimensions:</dt>
+					<dd>{imageObject.width} x {imageObject.height}px</dd>
+					<dt>Image Size:</dt>
+					<dd>{formatBytes(imageObject.imageBlob.size)}</dd>
+					<dt>Feature Count</dt>
+					<dd>{imageObject.features?.length}</dd>
+				</dl>
+			{/if}
+		</div>
+		<Viewer {imageObject} />
+	</div>
 {/if}
+
+<style>
+	.container {
+		display: flex;
+		flex-direction: column;
+		height: 100%;
+		position: relative;
+		gap: 1rem;
+	}
+
+	dl {
+		display: grid;
+		gap: 0.5rem 2rem;
+		grid-template-columns: auto 1fr;
+
+		& > * {
+			margin: 0;
+		}
+	}
+</style>
