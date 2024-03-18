33ce1804d948215dd54be202d6bc8e11d8e0294d
Simon Wiles <simonjwiles@gmail.com>
2024-03-17 20:59:24 -0700

    Add `<FeatureClip/>` component
    I'm not sure this is much better than a proof-of-concept at this point.

---
+++ src/components/FeatureClip.svelte
@@ -0,0 +1,72 @@
+<script>
+	import { tick, unstate } from 'svelte';
+	import { Loading } from 'carbon-components-svelte';
+
+	/**
+	 * @typedef {Object} FeatureClipProps
+	 * @property {ImageObject} imageObject Image object to view
+	 * @property {Feature} feature Feature to clip
+	 */
+
+	/** @type {FeatureClipProps} */
+	let { imageObject, feature } = $props();
+
+	let croppedImageBlob = $state();
+
+	/** @param {number[][]} coordinates */
+	const getRectangle = (coordinates) => {
+		return [
+			Math.min(...coordinates.map(([x]) => x)),
+			Math.min(...coordinates.map(([, y]) => y)),
+			Math.max(...coordinates.map(([x]) => x)),
+			Math.max(...coordinates.map(([, y]) => y))
+		];
+	};
+
+	$effect(() => {
+		if (!imageObject || !feature) return;
+
+		const vertices = feature.geometry.coordinates[0].map(([x, y]) => [x, 1 - y]);
+		const rect = getRectangle(unstate(vertices));
+		const height = rect[3] - rect[1];
+		const width = rect[2] - rect[0];
+
+		console.log(rect, height, width);
+
+		const img = new Image();
+		img.onload = () => {
+			// Note: using img.decode here was resulting in a weird race-condition-like error
+			//       whereby only one image (non-determinate) was decoding properly.
+			//       Using img.onload seems to work okay, though? 🤷
+			const canvas = document.createElement('canvas');
+			const ctx = canvas.getContext('2d');
+			canvas.width = width;
+			canvas.height = height;
+
+			ctx?.drawImage(img, rect[0], rect[1], width, height, 0, 0, width, height);
+			canvas.toBlob((blob) => {
+				croppedImageBlob = blob;
+			});
+		};
+
+		tick().then(() => (img.src = URL.createObjectURL(imageObject.imageBlob)));
+	});
+</script>
+
+{#if croppedImageBlob}
+	<img
+		src={URL.createObjectURL(croppedImageBlob)}
+		alt={`"${feature.properties.text}" (${imageObject.name})`}
+		loading="lazy"
+	/>
+{:else}
+	<Loading withOverlay={false} small />
+{/if}
+
+<style>
+	img {
+		max-height: 100%;
+		max-width: 100%;
+		height: 40px;
+	}
+</style>
