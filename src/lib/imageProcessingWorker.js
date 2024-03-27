3ba64e7b8d38725ec86ac1cd9da6421fbb770c33
Simon Wiles <simonjwiles@gmail.com>
2024-03-27 11:46:38 -0700

    Use `ImageBitmapRenderingContext` for performance
    This is better, but doesn't cure all ills, unfortunately.

--- src/lib/imageProcessingWorker.js
+++ src/lib/imageProcessingWorker.js
@@ -1,13 +1,13 @@
-(() => {
-	onmessage = async function (event) {
-		const { imageBlob, x, y, w, h, canvas } = event.data;
-		const port = event.ports[0];
-		const ctx = canvas.getContext('2d');
-		canvas.width = w;
-		canvas.height = h;
-		createImageBitmap(imageBlob).then((imageBitmap) => {
-			ctx.drawImage(imageBitmap, x, y, w, h, 0, 0, w, h);
-			port.postMessage('success');
-		});
-	};
-})();
+onmessage = async function (event) {
+	const { imageBlob, x, y, w, h, canvas } = event.data;
+	const port = event.ports[0];
+	canvas.width = w;
+	canvas.height = h;
+	const ctx = canvas.getContext('bitmaprenderer');
+	const imageBitmap = await createImageBitmap(imageBlob);
+	const croppedBitmap = await createImageBitmap(imageBitmap, x, y, w, h);
+	ctx.transferFromImageBitmap(croppedBitmap);
+	imageBitmap.close();
+	croppedBitmap.close();
+	port.postMessage('success');
+};
